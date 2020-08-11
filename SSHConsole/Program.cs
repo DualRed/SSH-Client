db4223aebacdd6b893c9e47c8f5fcaf45b1c3293
Dr.Red <baolinhkg1999@gmail.com>
2020-08-11 18:51:53 +0700

    Add project files.

---
+++ SSHConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Renci.SshNet;
+
+namespace SSHConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = @"localhost";
+            string username = "sshuser";
+            string password = @"Baolinhkg44";
+
+            using (var client = new SshClient(host, username, password))
+            {
+
+                //Start the connection
+                client.Connect();
+                Console.Write("Write command here: ");
+                String command = Console.ReadLine();
+                Console.WriteLine("-------------------");
+                Console.WriteLine("Result: ");
+                Console.WriteLine("-------------------");
+                SshCommand output = client.RunCommand(command);
+                client.Disconnect();
+                string answer = output.Result;
+                Console.WriteLine(answer);
+            }
+        }
+    }
+}
